902a948c6fb308b917d55f38ebadd46979c04710
Sven Helmberger <sven.helmberger@quinscape.de>
2018-06-14 15:35:40 +0200

    Fix tests
    Use @ContextConfiguration to find the contexts to test against.

--- src/test/java/de/quinscape/domainqlstarter/DomainqlstarterApplicationTests.java
+++ src/test/java/de/quinscape/domainqlstarter/DomainqlstarterApplicationTests.java
@@ -1,12 +1,17 @@
 package de.quinscape.domainqlstarter;
 
+import de.quinscape.domainqlstarter.runtime.DomainqlstarterApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = {
+	DomainqlstarterApplication.class
+})
 public class DomainqlstarterApplicationTests {
 
 	@Test
