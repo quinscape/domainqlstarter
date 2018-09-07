c09bac1b8a7e5e43c89eaa7ca55bcfaf5034b225
Sven Helmberger <sven.helmberger@quinscape.de>
2018-09-07 12:26:04 +0200

    Improve method handling

--- src/main/java/de/quinscape/domainqlstarter/runtime/config/AllowDevGraphQLAccess.java
+++ src/main/java/de/quinscape/domainqlstarter/runtime/config/AllowDevGraphQLAccess.java
@@ -4,16 +4,37 @@ import de.quinscape.domainqlstarter.runtime.controller.GraphQLController;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AllowDevGraphQLAccess
     implements RequestMatcher
 {
+    private static final Set<String> DEFAULT_IGNORED_METHODS;
+    static
+    {
+        Set<String> set = new HashSet<>();
+        set.add("GET");
+        set.add("HEAD");
+        set.add("TRACE");
+        set.add("OPTIONS");
+        DEFAULT_IGNORED_METHODS = set;
+    }
+
     private final boolean allowDevGraphQLAccess;
+
+    private final Set<String> ignoredMethods;
 
 
     public AllowDevGraphQLAccess(boolean allowDevGraphQLAccess)
     {
+        this(allowDevGraphQLAccess, DEFAULT_IGNORED_METHODS);
+    }
+
+    public AllowDevGraphQLAccess(boolean allowDevGraphQLAccess, Set<String> ignoredMethods)
+    {
         this.allowDevGraphQLAccess = allowDevGraphQLAccess;
+        this.ignoredMethods = ignoredMethods;
     }
 
 
@@ -21,7 +42,7 @@ public class AllowDevGraphQLAccess
     public boolean matches(HttpServletRequest request)
     {
         // we only protect POST requests
-        if (!request.getMethod().equals("POST"))
+        if (ignoredMethods.contains(request.getMethod()))
         {
             return false;
         }
